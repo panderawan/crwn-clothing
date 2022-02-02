60506d07bbd06dc8c2b00ecc715a3e70751655b9
Ruddy.A <panderawan@gmail.com>
2022-02-02 13:08:14 +0100

    fixed payment, sign-in, sign-off, persistence, session. Going to try to upload to heroku now

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,9 +2,15 @@ import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from '@redux-devtools/extension';
 
 import rootReducer from './root-reducer';
 import rootSaga from './root-saga';
+
+const composeEnhancers = composeWithDevTools({
+	trace: true,
+	traceLimit: 25
+});
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -14,7 +20,10 @@ if (process.env.NODE_ENV === 'development') {
 	middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+	rootReducer,
+	composeEnhancers(applyMiddleware(...middlewares))
+);
 
 sagaMiddleware.run(rootSaga);
 
