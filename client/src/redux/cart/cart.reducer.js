daf56e1866dc0471777e0bc8501642aeb5f9cbc9
panderawan <panderawan@gmail.com>
2023-10-14 17:03:46 +0200

    updated some modules, fixed firebase

--- client/src/redux/cart/cart.reducer.js
+++ client/src/redux/cart/cart.reducer.js
@@ -31,7 +31,16 @@ const cartReducer = (state = INITIAL_STATE, action) => {
 				)
 			};
 		case CartActionTypes.CLEAR_CART:
-			return { ...state, cartItems: [] };
+			return {
+				...state,
+				cartItems: []
+			};
+
+		case CartActionTypes.SET_CART_FROM_FIREBASE:
+			return {
+				...state,
+				cartItems: action.payload
+			};
 		default:
 			return state;
 	}
